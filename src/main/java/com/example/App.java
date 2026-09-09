1c654296bbcd52e5bddcac831cde9398937f36b7
Ubuntu <ubuntu@ip-172-31-4-1.ap-south-1.compute.internal>
2026-09-09 10:00:55 +0000

    Initial Java Docker application

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Team Java App!");
+        System.out.println("Version: 1.0");
+    }
+}
+
